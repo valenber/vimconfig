3e9e63307d172ee571bc7e22b68b938b3afa53c0
Valentin Berlin <valentin.berlin@protonmail.com>
2021-12-22 19:46:19 +0100

    fix(lsp code action): incorrect keymap

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -3,20 +3,20 @@ if not status_ok then
   return
 end
 
-local opts = { noremap = true }
+local opts = {noremap = true}
 local keymap = vim.api.nvim_set_keymap
 
-telescope.setup{
+telescope.setup {
   defaults = {
     prompt_prefix = " ",
     selection_caret = " ",
-    path_display = { "smart" },
+    path_display = {"smart"}
   }
 }
 
 keymap("n", "<C-p>", ":Telescope find_files theme=dropdown<CR>", opts)
 keymap("n", "<leader>gb", ":Telescope buffers theme=dropdown<CR>", opts)
-keymap("n", "<leader>ca", ":Telescope lsp_code_actions theme=cusor<CR>", opts)
+keymap("n", "<leader>ca", ":Telescope lsp_code_actions theme=cursor<CR>", opts)
 keymap("n", "<leader><Space>", ":Telescope live_grep<CR>", opts)
 keymap("n", "<leader>gr", ":Telescope lsp_references theme=dropdown<CR>", opts)
 keymap("n", "<leader>*", ":Telescope grep_string theme=dropdown<CR>", opts)
