42fab9d719ab617008e3d0ca32d8fd4581378b0f
Valentin Berlin <valentin.berlin@protonmail.com>
2022-01-04 22:54:29 +0100

    add autoformat on save

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -15,6 +15,11 @@ null_ls.setup({
 		formatting.stylelint,
 		diagnostics.eslint,
 	},
+	on_attach = function(client)
+		if client.resolved_capabilities.document_formatting then
+			vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+		end
+	end,
 })
 
 -- vim.api.nvim_exec(
